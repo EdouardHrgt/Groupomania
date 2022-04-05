3bb825e96a6c60119ea1108bb90ee81611ee3fe9
Edouard Herrengt <herrengt.edouard@gmail.com>
2022-04-05 18:14:18 +0200

    Dernier push avant rework UUID

--- backend/middleware/auth-mw.js
+++ backend/middleware/auth-mw.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
+    // dev branch
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN);
     const permission = decodedToken.permission;
